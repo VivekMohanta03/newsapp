82a67877f5910403775801a608f3e16da1888067
Vivek Kumar Mohanta <vivek123.mohanta@gmail.com>
2023-08-21 15:06:47 +0530

    basename added

--- src/App.js
+++ src/App.js
@@ -9,49 +9,49 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Router>
+        <Router basename="/newsapp">
           <Navbar />
           <Routes>
             <Route
               exact
-              path="/newsapp/"
+              path="/"
               element={<News key="general" pageSize={12} country="in" category="general" />}
             ></Route>
             <Route
               exact
-              path="/newsapp/business"
+              path="/business"
               element={<News key="business" pageSize={12} country="in" category="business" />}
             ></Route>
             <Route
               exact
-              path="/newsapp/entertainment"
+              path="/entertainment"
               element={
                 <News key="entertainment" pageSize={12} country="in" category="entertainment" />
               }
             ></Route>
             <Route
               exact
-              path="/newsapp/general"
+              path="/general"
               element={<News key="general" pageSize={12} country="in" category="general" />}
             ></Route>
             <Route
               exact
-              path="/newsapp/health"
+              path="/health"
               element={<News key="health" pageSize={12} country="in" category="health" />}
             ></Route>
             <Route
               exact
-              path="/newsapp/science"
+              path="/science"
               element={<News key="science" pageSize={12} country="in" category="science" />}
             ></Route>
             <Route
               exact
-              path="/newsapp/sports"
+              path="/sports"
               element={<News key="sports" pageSize={12} country="in" category="sports" />}
             ></Route>
             <Route
               exact
-              path="/newsapp/technology"
+              path="/technology"
               element={<News key="technology" pageSize={12} country="in" category="technology" />}
             ></Route>
           </Routes>
